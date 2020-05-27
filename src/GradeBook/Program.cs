8514134d48f90bf5a50af8c0fdc59aed4692fdc3
Frederik Desmet <fre_der_ik@outlook.com>
2020-05-27 08:34:16 +0200

    progress addition of flow structure and unit testing

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,15 +8,29 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Frederik's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+            
+            while (true)
+            {
+                Console.WriteLine("enter a grade or enter 'q' to quit:\n");
+                var input = Console.ReadLine();
+                
+                if (input == "q")
+                {
+                    break;
+                }
+                else
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+            }
 
 
             var stats = book.GetStatistics();
             Console.WriteLine($"The lowest grade is {stats.Low:N1}");
             Console.WriteLine($"The highest grade is {stats.High:N1}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
 
         }
     }
